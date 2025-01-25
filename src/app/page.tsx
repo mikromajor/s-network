73676bbcd87e513bbba21aae22ab56e25a5de62f
mikromajor <robocza.sowa@yandex.com>
2025-01-25 20:48:57 +0100

    feat   add ModeToggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/ModeToggle";
 import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
@@ -109,6 +110,7 @@ export default function Home() {
         <UserButton />
       </SignedIn>
       <Button variant={"secondary"}> Click me </Button>
+      <ModeToggle />
     </div>
   );
 }
