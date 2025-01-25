83ac22f6616bd5ac8c06560719c75ae3bfea1011
mikromajor <robocza.sowa@yandex.com>
2025-01-25 23:35:54 +0100

    feat   add Navbars for desktop & mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import Navbar from "@/components/Navbar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,14 +27,28 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang='en'>
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
-            {children}
-          </ThemeProvider>
-        </body>
+    <>
+      <html lang='en' suppressHydrationWarning>
+        <head />
+        <ClerkProvider>
+          <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+            <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+              <div className='min-h-screen'>
+                <Navbar />
+                <main className='py-8'>
+                  {/*container to center the content */}
+                  <div className='max-w-7xl mx-auto px-4'>
+                    <div className='grid grid-cols-1 lg:grid-cols-12 gap-6'>
+                      <div className='lg:block lg:col-span-3 hidden'>sidebar</div>
+                      <div className='lg:col-span-9'>{children}</div>
+                    </div>
+                  </div>
+                </main>
+              </div>
+            </ThemeProvider>
+          </body>
+        </ClerkProvider>
       </html>
-    </ClerkProvider>
+    </>
   );
 }
