ee6102d42343518acd7f9fb8d1c756080e4d3c6f
mikromajor <robocza.sowa@yandex.com>
2025-02-07 20:01:43 +0100

    feat   sync auth with db

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className='m-4'>
       Home page
